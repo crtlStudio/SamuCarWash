5a71b72b8b1442de47a883924d8d82333d9883c9
Pedro Leal <pedro.84leal@gmail.com>
2026-09-21 22:03:26 +0100

    usa fontsource para a fonte Ubuntu

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import '@fontsource/ubuntu/300.css'
+import '@fontsource/ubuntu/400.css'
+import '@fontsource/ubuntu/500.css'
+import '@fontsource/ubuntu/300-italic.css'
+import '@fontsource/ubuntu/400-italic.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
